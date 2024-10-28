7886ed082a0d4033e954bc698856fa79f30bdb40
Akshay <119416860+axai-kaizoku@users.noreply.github.com>
2024-10-28 12:17:28 +0530

    remove trpc (#2)

--- css.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "csstype" {
-  interface Properties {
-    "--tw-translate-y": string;
-  }
-}
--- src/server/actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import type { UserType } from "@/app/_components/UserForm";
-
-export const updateUserAction = async (data: UserType) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  console.log(data);
-};
