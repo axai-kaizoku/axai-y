925ce9471d434b2c136527469b79f605bf0471ff
Akshay <119416860+axai-kaizoku@users.noreply.github.com>
2024-11-24 12:20:41 +0530

    fix config file (#5)

--- next.config.js
+++ next.config.js
@@ -2,13 +2,43 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+
+/**
+ * await import("./src/env.js")
+ * import BundleAnalyzer from "@next/bundle-analyzer"
+ * const isProd = process.env.NODE_ENV === "production"
+ *
+ * /** @type {import("next").NextConfig} //
+ *
+ * const nextConfig = {
+ * typescript: {
+ *  ignoreBuildErrors: true,
+ * },
+ * eslint: {
+ *  ignoreDuringBuilds: true,
+ * },
+ * basePath: isProd ? "/axai-y" : "",
+ * assetPrefix: isProd ? "/axai-y" : "",
+ * }
+ *
+ * const withBundleAnalyzer = BundleAnalyzer({
+ * enabled: process.env.ANALYZE === "true",
+ * })
+ *
+ * export default withBundleAnalyzer(nextConfig)
+ */
+
+/**
+ *  This above config is for analyzing the build
+ *  (used in cases of optimization)
+ */
+
 await import("./src/env.js")
-import BundleAnalyzer from "@next/bundle-analyzer"
 
 const isProd = process.env.NODE_ENV === "production"
 
 /** @type {import("next").NextConfig} */
-const nextConfig = {
+const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -16,11 +46,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   basePath: isProd ? "/axai-y" : "",
-  assetPrefix: isProd ? "/axai-y" : "",
 }
 
-const withBundleAnalyzer = BundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-})
-
-export default withBundleAnalyzer(nextConfig)
+export default config
